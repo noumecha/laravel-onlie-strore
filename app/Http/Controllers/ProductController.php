9a110bb6ff04e6a7c14f72aa0b268d01189238b2
noumecha spaker <noumechaivan@gmail.com>
2023-06-23 08:40:34 +0100

    maj : jst-mj

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,34 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id"=>1, "name"=>"High Camera", "description"=>"Pro photography tools", "image"=>"camera.jpg", "price"=>"500000"],
-        ["id"=>2, "name"=>"PSP Console", "description"=>"The last ship Konsole for gamers", "image"=>"Konsole.jpg", "price"=>"300000"],
-        ["id"=>3, "name"=>"Graphic Tablet", "description"=>"Pro Designer Graphic tools", "image"=>"graphics.jpg", "price"=>"250000"],
-        ["id"=>4, "name"=>"Dark Alien Laptop", "description"=>"Laptop for Hackers-Gamers pro", "image"=>"dark_pc.jpg", "price"=>"600000"],
-        ["id"=>5, "name"=>"High Tech glasses", "description"=>"See the start en enjoy", "image"=>"optic_glasses.jpg", "price"=>"200000"],
-        ["id"=>6, "name"=>"IA Watch", "description"=>"Automate AI watch", "image"=>"ai_watch.jpg", "price"=>"100000"],
-        ["id"=>7, "name"=>"Spy Drone", "description"=>"Special spy drone", "image"=>"spy_drone.jpg", "price"=>"100000"],
-        ["id"=>8, "name"=>"Futurist phone", "description"=>"Futurist phone for enjoying tech", "image"=>"phone.jpg", "price"=>"100000"],
-    ];
 
     public function index()
     {
         $product_data = [];
         $product_data['title'] = "Online Store - Products";
         $product_data['subtitle'] = "List of Products";
-        $product_data['products'] = ProductController::$products;
+        $product_data['products'] = Product::all();
         return view('product.index')->with('product_data', $product_data);
     }
 
     public function show($id)
     {
         $product_data = [];
-        $product = ProductController::$products[$id-1];
+        //$product = ProductController::$products[$id-1];
+        $product = Product::findOrFail($id);
         $product_data['title'] = "Online Store - ".$product['name'];
         $product_data['subtitle'] = "Product information - ".$product['name'];
         $product_data['product'] = $product;
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -3,22 +3,24 @@
 @section('subtitle',$product_data['subtitle'])
 @section('content')
 <div class="card mb-3">
-    <div class="col-md-4">
-        <img src="{{ asset('/images/products/'.$product_data["product"]["image"]) }}" alt="" class="img-fluid rounded-start">
-    </div>
-    <div class="col-md-8">
-        <div class="card-body">
-            <h5 class="card-title">
-                {{ $product_data["product"]["name"]}} (XAF{{ $product_data["product"]["price"]}})
-            </h5>
-            <p class="card-text">
-                {{ $product_data["product"]["description"]}}
-            </p>
-            <p class="card-text">
-                <small class="text-muted">
-                    Add to Cart
-                </small>
-            </p>
+    <div class="row g-0">
+        <div class="col-md-6">
+            <img src="{{ asset('/images/products/'.$product_data["product"]["image"]) }}" alt="" class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-6">
+            <div class="card-body">
+                <h4 class="card-title">
+                    {{ $product_data["product"]["name"]}} (XAF{{ $product_data["product"]["price"]}})
+                </h4>
+                <div class="card-text">
+                    {{ $product_data["product"]["description"]}}
+                </div>
+                <div class="card-text">
+                    <small class="text-muted">
+                        Add to Cart
+                    </small>
+                </div>
+            </div>
         </div>
     </div>
 </div>
