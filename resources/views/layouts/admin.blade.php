be8ba91a5926031f6384e39777cbcd59a17b9611
noumecha spaker <noumechaivan@gmail.com>
2023-07-21 21:44:56 +0100

    maj : admin panel skeleton

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -61,5 +61,7 @@
             </div>
         </div>
         <!-- footer -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+        </script>
     </body>
 </html>
