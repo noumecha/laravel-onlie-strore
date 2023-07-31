9c2916c7e7f0405323e9c14c434bb0647ace0e02
noumecha spaker <noumechaivan@gmail.com>
2023-07-31 05:45:03 +0100

    upd : add middleware

--- app/Http/Middleware/AdminAuthMiddleware.php
+++ app/Http/Middleware/AdminAuthMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuthMiddleware
 {
@@ -16,6 +17,10 @@ class AdminAuthMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if(Auth::user() && Auth::user()->getRole()=='admin') {
+            return $next($request);
+        } else {
+            return redirect()->route('home.index');
+        }
     }
 }
